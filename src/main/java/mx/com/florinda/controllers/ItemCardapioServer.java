42f4cae05537382ed5bf68ddc3f6df1b34b87e87
Leandro Menezes <lmsilv@gmail.com>
2026-03-31 17:09:24 -0300

    Criação de um servidor http
    Criação de um HTTP Server `ItemCardapioServer.java` e classe para gerar um JSON `ItemCardapioService.java`.

---
+++ src/main/java/mx/com/florinda/controllers/ItemCardapioServer.java
@@ -0,0 +1,33 @@
+package mx.com.florinda.controllers;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ItemCardapioServer {
+
+    void main() throws IOException {
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(8000);
+        HttpServer server = HttpServer.create(inetSocketAddress, 0);
+
+        server.createContext("/itens-cardapio.json", exchange -> {;
+            String json = Files.readString(Path.of("itens-cardapio.json"));
+            byte[] bytes = json.getBytes();
+
+            exchange.getResponseHeaders().add("Content-Type", "application/json; charset=UTF-8");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.sendResponseHeaders(200, bytes.length);
+
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(bytes);
+        });
+
+        IO.println("Servidor iniciado!");
+        server.start();
+    }
+}
---
+++ src/main/java/mx/com/florinda/services/ItemCardapioService.java
@@ -0,0 +1,23 @@
+package mx.com.florinda.services;
+
+import com.google.gson.Gson;
+import mx.com.florinda.controllers.Cardapio;
+import mx.com.florinda.models.ItemCardapio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ItemCardapioService {
+
+    private static final Cardapio cardapio = new Cardapio("/databases/itens-cardapio.csv");
+
+    static void main() throws IOException {
+        List<ItemCardapio> itensCardapio = cardapio.getItensCardapio();
+        String itensJson = new Gson().toJson(itensCardapio);
+
+        Path path = Path.of("itens-cardapio.json");
+        Files.writeString(path, itensJson);
+    }
+}
